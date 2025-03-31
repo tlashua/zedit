55b2527e4c4cea6f679dcae534616faa6264c1d4
tim <tim@lashua.net>
2025-03-31 08:59:24 -0500

    wip

--- src/main/kotlin/net/lashua/zonedit/ui/RoomNode.kt
+++ src/main/kotlin/net/lashua/zonedit/ui/RoomNode.kt
@@ -3,19 +3,17 @@ package net.lashua.zonedit.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.Alignment
-import net.lashua.zonedit.model.Room
-import net.lashua.zonedit.model.Position
-import net.lashua.zonedit.model.ExitDirection
+import net.lashua.zonedit.model.*
 
 @Composable
 fun RoomNode(
@@ -24,7 +22,7 @@ fun RoomNode(
     selected: Boolean = false,
     onPositionChanged: (Position) -> Unit,
     onConnectionStarted: (Room, ExitDirection) -> Unit = { _, _ -> },
-    onClick: () -> Unit = {},
+    onClick: () -> Unit = {}
 ) {
     Box(
         modifier = modifier
@@ -34,15 +32,9 @@ fun RoomNode(
             )
             .padding(8.dp)
             .pointerInput(Unit) {
-                detectDragGestures(
-                    onDrag = { change, dragAmount ->
-                        change.consume()
-                        onPositionChanged(Position(
-                            x = room.position.x + dragAmount.x,
-                            y = room.position.y + dragAmount.y
-                        ))
-                    }
-                )
+                detectTapGestures {
+                    onClick()
+                }
             }
     ) {
         Column {
@@ -53,76 +45,30 @@ fun RoomNode(
             )
         }
 
-        // Show connection points when selected
         if (selected) {
-            // North connection point
-            Box(
-                modifier = Modifier
-                    .size(12.dp)
-                    .background(Color.Blue, CircleShape)
-                    .align(Alignment.TopCenter)
-                    .pointerInput(Unit) {
-                        detectDragGestures(
-                            onDragStart = { _ ->
-                                onConnectionStarted(room, ExitDirection.NORTH)
-                            },
-                            onDrag = { change, _ ->
-                                change.consume()
-                            }
-                        )
-                    }
-            )
-            // South connection point
-            Box(
-                modifier = Modifier
-                    .size(12.dp)
-                    .background(Color.Blue, CircleShape)
-                    .align(Alignment.BottomCenter)
-                    .pointerInput(Unit) {
-                        detectDragGestures(
-                            onDragStart = { _ ->
-                                onConnectionStarted(room, ExitDirection.SOUTH)
-                            },
-                            onDrag = { change, _ ->
-                                change.consume()
-                            }
-                        )
-                    }
-            )
-            // East connection point
-            Box(
-                modifier = Modifier
-                    .size(12.dp)
-                    .background(Color.Blue, CircleShape)
-                    .align(Alignment.CenterEnd)
-                    .pointerInput(Unit) {
-                        detectDragGestures(
-                            onDragStart = { _ ->
-                                onConnectionStarted(room, ExitDirection.EAST)
-                            },
-                            onDrag = { change, _ ->
-                                change.consume()
-                            }
-                        )
-                    }
-            )
-            // West connection point
-            Box(
-                modifier = Modifier
-                    .size(12.dp)
-                    .background(Color.Blue, CircleShape)
-                    .align(Alignment.CenterStart)
-                    .pointerInput(Unit) {
-                        detectDragGestures(
-                            onDragStart = { _ ->
-                                onConnectionStarted(room, ExitDirection.WEST)
-                            },
-                            onDrag = { change, _ ->
-                                change.consume()
-                            }
-                        )
-                    }
-            )
+            listOf(
+                ExitDirection.NORTH to Alignment.TopCenter,
+                ExitDirection.SOUTH to Alignment.BottomCenter,
+                ExitDirection.EAST to Alignment.CenterEnd,
+                ExitDirection.WEST to Alignment.CenterStart
+            ).forEach { (direction, alignment) ->
+                Box(
+                    modifier = Modifier
+                        .size(12.dp)
+                        .background(Color.Blue, CircleShape)
+                        .align(alignment)
+                        .pointerInput(Unit) {
+                            detectDragGestures(
+                                onDragStart = { _ ->
+                                    onConnectionStarted(room, direction)
+                                },
+                                onDrag = { change, _ ->
+                                    change.consume()
+                                }
+                            )
+                        }
+                )
+            }
         }
     }
 }
